c452e3d76cf92875d32861653f33e9591a6c49a2
JunyoungChoi92 <124121994+JunyoungChoi92@users.noreply.github.com>
2023-03-29 20:47:16 +0900

    Update AskQuestionBox.js

--- frontend/src/components/chat/AskQuestionBox.js
+++ frontend/src/components/chat/AskQuestionBox.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import * as chatGPTActions from "@/lib/store/modules/chatGPT";
 import { useDispatch, useSelector } from "react-redux";
 
+// This code defines the AskQuestion component which renders a text input field and a button for asking a follow-up question.
 export default function AskQuestion() {
     const { showInteractionPanel } = useSelector((state) => state.map);
+    // The component uses the useState hook to manage the state of the followupQuestion variable, which stores the text entered into the input field by the user.
     const [followupQuestion, setFollowupQuestion] = useState("");
     const dispatch = useDispatch();
-
+    
+    // The component returns a div containing an input field and a button. When the button is clicked, the followupQuestion variable is dispatched to the chatGPT store module.
+    // The component also dispatches the setMod action to update the state of the content that the ChatBox component show.
     return (
         <div
             style={{
